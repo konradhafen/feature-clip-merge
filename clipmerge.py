915c5da4ddbb9a14aecac6ffaee6af4ffff55976
KONRADSPC\khafe <konrad.hafen@gmail.com>
2018-02-12 14:11:35 -0800

    clip by features working

--- clipmerge.py
+++ clipmerge.py
@@ -1,15 +1,46 @@
 import os
 from osgeo import gdal, ogr
 
-def clipRasterWithPolygon(rasterpath, polygonpath, outputpath, nodata=-9999, xres=None, yres=None, field = None, fieldValue = None):
-    if xres is None or yres is None:
-        xres, yres = getXYResolution(rasterpath)
+def clipByFeature(inputdir, outputdir, rasterfiles, shapefile, fieldname, nodata=-9999, xres=None, yres=None):
+    """
+    Clip multiple rasters by features in a shapefile. Creates a new directory for each feature and saves clipped rasters in the directory
 
-    wexp = str(field) + " = " + str(fieldValue)
-    warpOptions = gdal.WarpOptions(format='GTiff', cutlineDSName=polygonpath, cropToCutline=True, cutlineWhere=wexp, xres=xres, yres=abs(yres),
-                                   dstNodata=nodata)
+    Args:
+        inputdir: directory where rasters to be clipped are located
+        outputdir: directory where a new directory for each clipped raster will be created
+        rasterfiles: list of raster file names and extensions to be clipped. e.g. ['raster1.tif', 'raster2.tif]
+        shapefile: shapefile containing features to clip with
+        fieldname: name of field to select feature with
+        nodata: nodata value (default: -9999)
+        xres: x resolution of output raster (default: None), with default resolution is taken from the input rater
+        yres: y resolution of output rater (defaul: None), with default resolution is taken from the input rater
+
+    Returns:
+        None
+
+    """
+    fieldValues, fids = getFieldValues(shapefile, fieldname)
+    print fieldValues
+    for value in fieldValues:
+        dirvalue = outputdir + "/" + str(value)
+        print value
+        if not os.path.isdir(dirvalue):
+            os.mkdir(dirvalue)
+        for raster in rasterfiles:
+            infile = inputdir + "/" + str(raster)
+            if os.path.exists(infile):
+                clipRasterWithPolygon(infile, shapefile, dirvalue + "/" + str(raster), nodata=nodata, xres=xres, yres=yres,
+                                      fieldValue=value, field=fieldname)
+
+def clipRasterWithPolygon(rasterpath, polygonpath, outputpath, nodata=-9999, xres=None, yres=None, field=None, fieldValue=None):
+    if xres is None or yres is None: xres, yres = getXYResolution(rasterpath)
+    if field is not None and fieldValue is not None: wexp = str(field) + " = \'" + str(fieldValue) + "\'"
+    else: wexp = None
+    print "where expression", wexp
+
+    warpOptions = gdal.WarpOptions(format='GTiff', cutlineDSName=polygonpath, cropToCutline=True, cutlineWhere=wexp, xRes=xres, yRes=abs(yres), dstNodata=nodata)
+    gdal.WarpOptions()
     gdal.Warp(outputpath, rasterpath, options=warpOptions)
-
     return None
 
 def getFieldValues(dataset, fieldName):
@@ -70,3 +101,13 @@ def getXYResolution(rasterPath):
         return geot[1], geot[5]
     else:
         return None
+
+
+#RUN HERE
+
+shapefile ="C:/temp/testclip/huc12.shp"
+indir = "C:/temp/testclip"
+outdir = "C:/temp/testclip/clips"
+rasternames = ["dem.tif"]
+
+clipByFeature(indir, outdir, rasternames, shapefile, fieldname="HUC12")
